2571da0bee0dea287757d19ab9a6b2c879c3437d
jrigottiedu <juan.rigotti@bue.edu.ar>
2025-06-12 00:29:30 -0300

    update

--- src/pages/semana12/components/Semana12Afterclass.jsx
+++ src/pages/semana12/components/Semana12Afterclass.jsx
@@ -4,65 +4,35 @@ const Semana12Afterclass = () => {
   return (
     <div>
       <h1 className="title-afterclass">After Class | Miércoles 11 de junio</h1>
-      {/* <section className="home-info-container">
-        <p className='font-bold pb-2'>Resolvimos el cuestionario de la clase 10 y los ejercicios!</p>
+      <section className="home-info-container">
+        <p className='font-bold pb-2'>Taller armando la Pre-entrega</p>
 
         <a
-          href="https://www.youtube.com/watch?v=vXLDkEMPULY"
+          href="https://www.youtube.com/watch?v=uSu5qg9xW-4"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center gap-2 no-underline text-gray-700 hover:text-red-600 transition-colors duration-200 pb-5"
         >
           <span className="material-icons text-red-600 group-hover:text-red-700">ondemand_video</span>
-          <span>Video de clase grabada</span>
+          <span>Video con la grabación del taller</span>
         </a>
 
 
         <div className='bg-gray-100 p-2 border border-gray-400 rounded-xl mb-10 w-full'>
-          <p className='font-bold pb-2'>Resolución del cuestionario clase 10</p>
+          <p className='font-bold pb-2'>Desarrollo de la actividad</p>
 
           <p className='font-semibold'>Temas destacados</p>
           <ul className='list-disc list-inside'>
-            <li>Docstrings: es un comentario que se agrega a la función y nos sirve de ayuda junto con la función help()</li>
-            <li>Las funciones pueden acceder a las variables globales, pero si queremos modificar su valor desde la función, debemos utilizar la palabra global</li>
-            <li>En Python, los argumentos se pasan por referencia, esto significa que se pasa la posición de memoria de la variable, no el valor </li>
-            <li>Cuando pasamos a una función una lista, como se pasa su referencia, la función modifica su contenido y no hace falta retornarla</li>
-            <li><a href="https://www.youtube.com/watch?v=vXLDkEMPULY" target='_blank' className='link'>Ver video hasta minuto 43</a></li>
-            <li><a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_11/after_class/cuestionario.py" target='_blank' className='link'>Ver código de soporte</a></li>
+            <li>Modelamos un gestor de inventario de los vehículos de una concesionaria</li>
+            <li>La consigna esta en línea con lo solicitado para la Pre-entrega</li>
+            <li>Hicimos un análisis de los requerimentos y evaluamos las herramientas a utilizar para cada parte del proyecto</li>
+            <li>Trabajamos desde el VSCode generando y ajustando el código</li>
+            <li><a href="https://docs.google.com/presentation/d/17o41Pp-iprRV60Ry84FYiqWZ-V14punppkAMedu36b8/edit?usp=sharing" target='_blank' className='link'>PPT - Presentación de base</a></li>
+            <li><a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_12/after_class/pre_entrega_modelo.py" target='_blank' className='link'>Ver código de soporte</a></li>
           </ul>
         </div>
 
-        <div className='bg-gray-100 p-2 border border-gray-400 rounded-xl mb-10 w-full'>
-          <p className='font-bold pb-2'>Resolvimos el Ejercicio 1 - getNombre() <a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_11/after_class/Ejercicio_tarea_1_resolucion.py" target='_blank' className='link'>Ver código</a></p>
-
-          <ul className='list-disc list-inside'>
-            <li>Desarrollamos el cuerpo de la función que solicita y valida el nombre del producto y lo retorna</li>
-            <li>Luego creamos una función más generica que se puede usar para obtener cualquier string/cadena y reutilizar código de manera más eficiente</li>
-          </ul>
-        </div>
-
-        <div className='bg-gray-100 p-2 border border-gray-400 rounded-xl mb-10 w-full'>
-          <p className='font-bold pb-2'>Resolvimos el Ejercicio 2 - getPrecio() <a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_11/after_class/Ejercicio_tarea_2_resolucion.py" target='_blank' className='link'>Ver código</a></p>
-
-          <ul className='list-disc list-inside'>
-            <li>Reutilizamos el código de la pre-entrega para armar la función</li>
-          </ul>
-        </div>
-
-        <div className='bg-gray-100 p-2 border border-gray-400 rounded-xl mb-10 w-full'>
-          <p className='font-bold pb-2'>Resolvimos el Ejercicio 4 - buscarProducto() <a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_11/after_class/Ejercicio_tarea_4_resolucion.py" target='_blank' className='link'>Ver código</a></p>
-
-          <ul className='list-disc list-inside'>
-            <li>Partimos del código de la pre-entrega</li>
-            <li>Usamos lista_productos como una variable global</li>
-            <li>Pasamos como argumento el nombre del producto a buscar</li>
-            <li>Vimos como usar return para terminar la función si hay coincidencia</li>
-          </ul>
-        </div>
-
-
-
-      </section> */}
+             </section>
     </div>
   )
 }
