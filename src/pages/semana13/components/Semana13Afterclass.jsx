41ccd6a911e4b5b11a063b6ebb8cb7e06e920714
jrigottiedu <juan.rigotti@bue.edu.ar>
2025-06-18 23:17:14 -0300

    update

--- src/pages/semana13/components/Semana13Afterclass.jsx
+++ src/pages/semana13/components/Semana13Afterclass.jsx
@@ -4,35 +4,32 @@ const Semana13Afterclass = () => {
   return (
     <div>
       <h1 className="title-afterclass">After Class | Miércoles 18 de junio</h1>
-      {/* <section className="home-info-container">
-        <p className='font-bold pb-2'>Taller armando la Pre-entrega</p>
+      <section className="home-info-container">
+        <p className='font-bold pb-2'>Repaso: desde funciones 101 hasta Base de datos! + PFI</p>
 
         <a
-          href="https://www.youtube.com/watch?v=uSu5qg9xW-4"
+          href="https://youtu.be/V3lyBdFFYXM"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center gap-2 no-underline text-gray-700 hover:text-red-600 transition-colors duration-200 pb-5"
         >
           <span className="material-icons text-red-600 group-hover:text-red-700">ondemand_video</span>
-          <span>Video con la grabación del taller</span>
+          <span>Video After Class</span>
         </a>
 
 
         <div className='bg-gray-100 p-2 border border-gray-400 rounded-xl mb-10 w-full'>
-          <p className='font-bold pb-2'>Desarrollo de la actividad</p>
-
           <p className='font-semibold'>Temas destacados</p>
           <ul className='list-disc list-inside'>
-            <li>Modelamos un gestor de inventario de los vehículos de una concesionaria</li>
-            <li>La consigna esta en línea con lo solicitado para la Pre-entrega</li>
-            <li>Hicimos un análisis de los requerimentos y evaluamos las herramientas a utilizar para cada parte del proyecto</li>
-            <li>Trabajamos desde el VSCode generando y ajustando el código</li>
-            <li><a href="https://docs.google.com/presentation/d/17o41Pp-iprRV60Ry84FYiqWZ-V14punppkAMedu36b8/edit?usp=sharing" target='_blank' className='link'>PPT - Presentación de base</a></li>
-            <li><a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_12/after_class/pre_entrega_modelo.py" target='_blank' className='link'>Ver código de soporte</a></li>
+            <li><span className='font-semibold'>Entrega final PFI:</span> abre el 30 de junio (en principio cierra el 14 de julio)</li>
+            <li>Si bien pueden refactorizar el código de la pre-entrega, para el armado de la entrega final vamos a trabajar sobre este código base <a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/entrega-final/2_pfi_estructura_.py" target="_blank" className='link'>Ver código</a></li>
+            <li>Hicimos un recorrido de los temas vistos durante las últimas tres clases</li>
+            <li>Funciones en todas sus variantes, con y sin argumento, con y sin retorno. Agregamos algo de try-except para manejo de errores <a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_13/after_class/repaso_funciones.py" target='_blank' className='link'>Ver código</a> </li>
+            <li>También repasamos los conceptos de Base de datos, tablas, campos y registros. Mencionamos los tipos de datos y atributos y por último exploramos la última tabla "productos" que creamos en el DB Browser.</li>
           </ul>
         </div>
 
-      </section> */}
+      </section>
     </div>
   )
 }
