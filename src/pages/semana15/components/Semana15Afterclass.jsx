5abd345a2a64af038262931476bb1ca786b0f94a
jrigottiedu <juan.rigotti@bue.edu.ar>
2025-07-03 23:40:02 -0300

    update

--- src/pages/semana15/components/Semana15Afterclass.jsx
+++ src/pages/semana15/components/Semana15Afterclass.jsx
@@ -4,11 +4,11 @@ const Semana15Afterclass = () => {
   return (
     <div>
       <h1 className="title-afterclass">After Class | Miércoles 2 de julio</h1>
-      {/* <section className="home-info-container">
-        <p className='font-bold pb-2'>Alta de producto integrado con Base de datos!</p>
+      <section className="home-info-container">
+        <p className='font-bold pb-2'>Refactorizamos el modelo de la pre-entrega para integrar con BD!</p>
 
         <a
-          href="https://www.youtube.com/watch?v=Nepb9FpFUzI"
+          href="https://youtu.be/u8Ojeo_pfwI"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center gap-2 no-underline text-gray-700 hover:text-red-600 transition-colors duration-200 pb-5"
@@ -21,21 +21,15 @@ const Semana15Afterclass = () => {
         <div className='bg-gray-100 p-2 border border-gray-400 rounded-xl mb-10 w-full'>
           <p className='font-semibold'>Temas destacados</p>
           <ul className='list-disc list-inside'>
-            <li>Respondimos el cuestionario de clase 12: viene con preguntas muy interesantes de módulos, bloques try-except y manejo de archivos</li>
-            <li>Aunque las respuestas puedan no aparecer en el mismo orden, ver código de soporte <a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_14/after_class/cuestionario_6.py" target="_blank" className='link'>Ver en Github</a></li>
-            <li>Resolvimos el ejercicio de la clase 13, que consiste el integrar el alta de productos con Sqlite</li>
-            <li>Reutilizamos las funciones db_crear_tabla_producto(), db_insertar_producto(), db_leer_productos()</li>
-            <li>Exploramos que sucede si insertamos registros con campos vacios o datos no unicos</li>
-            <li><a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_14/after_class/ejercicio.py" target='_blank' className='link'>Ver código en Github</a></li>
-            <li>Recuerden que siempre es buena práctica armar diagramas de flujo y/o pseudocódigo antes de comenzar a programar</li>
-            <li>Aunque no llegamos con el tiempo, también se sugiere armar gráficos de entidad/relación previo a armar la estructura de la base de datos</li>
-            <li>Podemos usar estas plataformas web:</li>
-            <li className='pl-2'><a href="https://draw.io/" target='_blank' className='link'>Draw.io</a></li>
-            <li className='pl-2'><a href="https://www.lucidchart.com/" target='_blank' className='link'>Lucidchart</a></li>
+            <li>Tomamos como punto de partida el modelo de la pre-entrega y comenzamos a refactorizar el código para integrar con Sqlite3 <a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_15/After%20Class/main.py" target='_blank' className='link'>Ver en Github</a></li>
+            <li>En el archivo bd_metodos comenzamos a incorporar las funciones que manejan la base <a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_15/After%20Class/bd_metodos.py" target="_blank" className='link'>Ver en Github</a></li>
+            <li>Sugerimos normalizar un retorno de los métodos para informar al usuario si la acción sobre la base fue exitosa o no</li>
+            <li>Insertamos en la tabla productos tipos de datos no permitidos, y vimos que Sqlite3 los acepta, por lo cual se sugiere reforzar la validación del tipo de dato antes de insertarlo </li>
+            <li>Introducimos dos lineas de código para generar una ruta a la bd que no dependa del directorio desde donde se corre el main. Ver lineas 5 a 9 del archivo bd_metodos <a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_15/After%20Class/bd_metodos.py"></a>Ver en Github</li>
           </ul>
         </div>
 
-      </section> */}
+      </section>
     </div>
   )
 }
