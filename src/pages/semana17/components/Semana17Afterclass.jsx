96bffd25a6b4fa4f81ca6c46f17ce09e0bf5c896
jrigottiedu <juan.rigotti@bue.edu.ar>
2025-07-16 23:49:12 -0300

    update

--- src/pages/semana17/components/Semana17Afterclass.jsx
+++ src/pages/semana17/components/Semana17Afterclass.jsx
@@ -7,8 +7,8 @@ const Semana17Afterclass = () => {
       <section className="home-info-container">
         <p className='font-bold pb-2'>Taller Entrega Final</p>
 
-        {/* <a
-          href="https://youtu.be/u8Ojeo_pfwI"
+        <a
+          href="https://www.youtube.com/watch?v=v9lujcWq6Ks&list=PLR6qrffywxhDoihz7VcDGyNbVnksVtXnx&index=1"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center gap-2 no-underline text-gray-700 hover:text-red-600 transition-colors duration-200 pb-5"
@@ -21,13 +21,13 @@ const Semana17Afterclass = () => {
         <div className='bg-gray-100 p-2 border border-gray-400 rounded-xl mb-10 w-full'>
           <p className='font-semibold'>Temas destacados</p>
           <ul className='list-disc list-inside'>
-            <li>Tomamos como punto de partida el modelo de la pre-entrega y comenzamos a refactorizar el código para integrar con Sqlite3 <a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_15/After%20Class/main.py" target='_blank' className='link'>Ver en Github</a></li>
-            <li>En el archivo bd_metodos comenzamos a incorporar las funciones que manejan la base <a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_15/After%20Class/bd_metodos.py" target="_blank" className='link'>Ver en Github</a></li>
-            <li>Sugerimos normalizar un retorno de los métodos para informar al usuario si la acción sobre la base fue exitosa o no</li>
-            <li>Insertamos en la tabla productos tipos de datos no permitidos, y vimos que Sqlite3 los acepta, por lo cual se sugiere reforzar la validación del tipo de dato antes de insertarlo </li>
-            <li>Introducimos dos lineas de código para generar una ruta a la bd que no dependa del directorio desde donde se corre el main. Ver lineas 5 a 9 del archivo bd_metodos <a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_15/After%20Class/bd_metodos.py"></a>Ver en Github</li>
+            <li>PPT - Resumen de concpetos para la Entrega Final <a href="https://docs.google.com/presentation/d/1E6ETrkQ-bZdPCAR6eKt8pOeEyrUoJYvNLp01fWPKP-o/edit?usp=sharing" target='_blank' className='link'>Ver PPT</a></li>
+            <li>Exploramos un archivo pre-armado con las funciones/métodos para manejar la BD en Sqlite <a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_17/afterclass/bd_metodos.py" target="_blank" className='link'>Ver en Github</a></li>
+            <li>Exploramos un archivo pre-armado con las funciones/métodos para ingreso de datos <a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_17/afterclass/get_metodos.py" target="_blank" className='link'>Ver en Github</a></li>
+            <li>Refactorizamos el código modelo de la pre-entrega de manera de desarrollar todas las funcionalidades del menú <a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_17/afterclass/pfi_modelo_vehiculos.py" target="_blank" className='link'>Ver en Github</a></li>
+            <li>Carpeta de Github con todos los archivos de hoy <a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/tree/master/semana_17/afterclass" target='_blank' className='link'>Ver en Github</a></li>
           </ul>
-        </div> */}
+        </div>
 
       </section>
     </div>
