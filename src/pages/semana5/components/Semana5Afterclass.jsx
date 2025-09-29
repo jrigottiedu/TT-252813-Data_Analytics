eaff12d9a48810ee72a31c24994f387c375f19ba
jrigottiedu <juan.rigotti@bue.edu.ar>
2025-09-28 21:21:39 -0300

    update with after class 5

--- src/pages/semana5/components/Semana5Afterclass.jsx
+++ src/pages/semana5/components/Semana5Afterclass.jsx
@@ -4,19 +4,33 @@ const Semana5Afterclass = () => {
   return (
     <div>
       <h1 className="title-afterclass">After Class | Miércoles 24 de septiembre</h1>
-      {/* <section className="home-info-container">
-        <p>Dedicamos una parte del After a responder consultas sobre métodos (en particular isdigit aplicado a bloques condicionales) y el uso del operador lógico not <a href='https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_5/after_class/consultas.py' target="_blank" className='link'>Ver código aquí</a></p>
-        <p>Resolvimos el cuestionario 2 de la clase 4</p>
+      <section className="home-info-container">
+        <h2 className='title-h2'>Resumen del After Class</h2>
         <ul className='list-disc list-inside'>
-          <li><a href='https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_5/after_class/cuestionario.py' target='_blank' className='link'>Ver codigo con anotaciones importantes</a></li>
-          <li><a href="https://www.youtube.com/watch?v=avxXXV6U2-I" target='_blank' className='link'>Ver video con resolución - Hacer click en minuto 00:43:00 en la descripción</a></li>
+          <li>Resolvimos las actividades 1 y 2 de la clase 5 sobre identificar y tratar registros duplicados y valores nulos</li>
+          <li>Los datasets para esta actividad se encuentran en un Google Sheet por lo que se importan desde el Colab</li>
+          <li><a href="https://github.com/jrigottiedu/TT-2C2025-Data-Analitycs-Notebooks/blob/main/After_class_5/After_class_5_tratamiento_duplicados_nulos.ipynb" target='_blank' className='link'>Ver Notebook</a></li>
         </ul>
-        <p>Resolvimos el ejercicio de la clase 4</p>
+
+
+        <h2 className='title-h2 pt-10'>Recordatorios</h2>
         <ul className='list-disc list-inside'>
-          <li><a href='https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_5/after_class/ejercicio.py' target='_blank' className='link'>Ver codigo con anotaciones importantes</a></li>
-          <li><a href="https://www.youtube.com/watch?v=avxXXV6U2-I" target='_blank' className='link'>Ver video con resolución - Hacer click en minuto 01:32:00 en la descripción</a></li>
+          <li>Nos encontramos nuevamente el Lunes 29/9 a las 19:00 hs por Zoom, tienen el Link en la page Inicio</li>
         </ul>
-      </section> */}
+
+        {/* Grabación de After Class en Youtube */}
+        <div className='py-5'>
+          <h2 className='title-h2'>Grabación del After Class 5</h2>
+          <a
+            className="flex items-center gap-2 no-underline text-gray-700 hover:text-darkblue-600 transition-colors duration-200 pb-5"
+            href='https://www.youtube.com/watch?v=qSy5K-Z0BFM'
+            alt="Video semana 5 After Class "
+            target="_blank">
+            <span className="material-icons text-darkblue-600 group-hover:text-darkblue-700">ondemand_video</span>
+            <span>Ver en Youtube</span>
+          </a>
+        </div>
+      </section>
     </div>
   )
 }
