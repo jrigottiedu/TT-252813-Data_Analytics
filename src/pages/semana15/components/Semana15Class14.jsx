0c44de7dc3cfcb9670aee70ec03883681224e86a
jrigottiedu <juan.rigotti@bue.edu.ar>
2025-12-03 22:25:00 -0300

    update clase after 15

--- src/pages/semana15/components/Semana15Class14.jsx
+++ src/pages/semana15/components/Semana15Class14.jsx
@@ -3,40 +3,39 @@ import React from 'react'
 const Semana15Class14 = () => {
   return (
     <div>
-      <h1 className="title-class">Clase 15 | Lunes 30 de junio</h1>
+      <h1 className="title-class">Clase 15 | Lunes 1 de diciembre</h1>
 
       <section className="home-info-container">
 
         {/* Resumen de clase */}
         <div>
           <h2 className='title-h2'>Resumen de clase</h2>
-          <p className='pt-2'>Escribimos todas las consultas SQL desde Python!</p>
+          <p className='pt-2'>En nuestra clase de hoy vimos Regresión Lineal</p>
           <ul className='list-disc list-inside'>
-            <li>Hoy completamos las funciones que nos permiten: </li>
-            <li className="pl-2">Leer producto por id</li>
-            <li className="pl-2">Leer productos por nombre</li>
-            <li className="pl-2">Actualizar el precio de un registro según el id</li>
-            <li className="pl-2">Eliminar un producto a partir de su id</li>
-            <li><a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/semana_15/clase_14/2_bd_metodos.py" target="_blank" className='link'>Metodos bd</a> </li>
-            <li>Tengan a mano las consultas SQL más usadas <a href="https://drive.google.com/file/d/1zNsMyqoDwCAv9tlkPusWHwZa3hI-v72O/view" target='_blank' className='link'>Ver PDF con consultas SQL</a></li>
-          </ul>
-          <p className='pt-5 font-semibold'>Luego trabajamos con la entrega final</p>
-          <ul className='list-disc list-inside'>
-            <li>Ver PPT a partir del Slide 25 "Entrega de Proyecto" con la consigna <a href="https://docs.google.com/presentation/d/1Nv7EBmhtGzUecTDci3sYHObt8epqdQA0/edit?usp=sharing&ouid=115334316874644775342&rtpof=true&sd=true" target='_blank' className='link'>Ver PPT</a></li>
-            <li>Comenzamos a desarrollar el código en el archivo principal <a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/entrega-final/2_pfi_estructura_.py" target='_blank' className='link'>Ver código Estructura PFI</a></li>
-            <li>Copiamos, pegamos y adaptamos las funciones vistas en las últimas dos clases (crear tabla productos, insertar producto y eliminar producto) <a href="https://github.com/jrigottiedu/TT-1C2025-25011-codigo-de-clases/blob/master/entrega-final/bd_metodos.py" target='_blank' className='link'>Ver código métodos bd</a></li>
+            <li>Generamos un modelo de regresión lineal para predecir el peso de una persona dada su altura</li>
+            <li>Y luego lo extendimos a un dataset de precios de propiedades en Amsterdam, para predecir el precio de la propiedad dada la superficie</li>
+            <li>Ver la teoría básica en este PPT <a href="https://docs.google.com/presentation/d/19iVSzdec7M973RDjXMVBGAUjj_FAdnrH/edit?usp=sharing&ouid=115334316874644775342&rtpof=true&sd=true" target='_blank' className='link'>PPT Regresión Lineal</a></li>
+            <li>Ver Notebook en Google Colab con caso práctico y caso real <a href="https://colab.research.google.com/drive/1vidt-5e2GwPea9K7PGUGPUXiH_YR6k0t#scrollTo=qYukJf5WVSeq" target='_blank' className='link'>Notebook Regresión en Colab</a></li>
+            <li>Ver Notebook en Google Colab con caso práctico y caso real <a href="https://github.com/jrigottiedu/TT-2C2025-Data-Analitycs-Notebooks/blob/main/_26_Clase_15/Clase_15.ipynb" target='_blank' className='link'>Notebook Regresión en Github</a></li>
+            <li>Los conceptos básicos son:</li>
+            <li>EDA al dataset, y pre-procesamiento (eliminar nulos, transformar datos, etc)</li>
+            <li>Separar el Dataset en un conjunto de entreamiento y otro de testing</li>
+            <li>Entrenar un modelo de regresión lineal con el conjunto de entreamiento</li>
+            <li>Evaluar las métricas, R2, MSE, RMSE usando el conjunto de testing</li>
+            <li>Escribir conclusiones, que tan bueno es el modelo y las limitaciones que tiene</li>
           </ul>
         </div>
 
         {/* Grabación de Clase en Youtube */}
-        <div>
-          <h2 className='title-h2'>Video grabado</h2>
+        <div className='py-5'>
+          <h2 className='title-h2'>Grabación Clase 15</h2>
           <a
-            className='link'
-            href='https://www.youtube.com/watch?v=UigGf2Q-mEE'
-            alt="Video semana 15"
+            className="flex items-center gap-2 no-underline text-gray-700 hover:text-darkblue-600 transition-colors duration-200 pb-5"
+            href='https://www.youtube.com/watch?v=9zieYKFlR7A'
+            alt="Video semana 15 Class 15"
             target="_blank">
-            Ver aquí en Youtube
+            <span className="material-icons text-darkblue-600 group-hover:text-darkblue-700">ondemand_video</span>
+            <span>Ver en Youtube</span>
           </a>
         </div>
 
@@ -44,47 +43,17 @@ const Semana15Class14 = () => {
         <div>
           <h2 className='title-h2'>Recordatorios</h2>
           <ul className='list-disc list-inside'>
-            <li>Resolver el cuestionario de clase 12 en el Campus Virtual - encuentran la resolución en el video del after class <a href="https://www.youtube.com/watch?v=Nepb9FpFUzI" target='_blank' className='link'>Ver aquí</a></li>
-            <li>A partir de hoy pueden comenzar a subir el PFI al campus!</li>
-            <li>Nos reencontramos el miércoles 2 de julio en el After Class</li>
+            <li><span className='font-semibold'>"Entrega Final":</span> 1ra fecha sugerida hasta el 8/12 - 2da y última fecha límite hasta el 15/12</li>
+            <li>En el After Class de este miércoles 19 vamos a analizar el caso de estudio </li>
+            <li>*** Lunes 8/15 - Feriado ***</li>
           </ul>
         </div>
 
-        {/* Miscelaneas
-                <div>
-                    <h2 className='title-h2'>Miscelaneas</h2>
-                    <p>Aqui les comparto un video complementario que explica como utilizar el modo Debug</p>
-                    <ul className='list-disc list-inside'>
-                        <li><strong>Video 1: </strong> Utilizando el Modo Debug <a className="link" href="https://youtu.be/spYjDGDKvTM" target='_blank'>Ver en Youtube</a></li>
-                    </ul>
-                </div> */}
-
-        {/* Temas Clase 15 */}
-        <div>
-          <h2 className='title-h2'>Temas a ver en Clase 15 - lunes 7 de julio</h2>
-          <ul className='list-disc list-inside'>
-            <li>Vamos a completar el ejemplo modelo del PFI</li>
-            <li>Veremos en detalle los temas pendientes de la clase 14</li>
-            <li>Profundizaremos operaciones CRUD</li>
-            <li>Analizaremos Inyecciones SQL para temas de seguridad</li>
-            <li>Veremos manejo de transacciones y control de errores</li>
-            <li>Espacio para dudas sobre la Entrega final</li>
-            <li>Dado que el miércoles 9 es feriado, proponemos un espacio para consultas el sábado 12 (el horario lo conversamos en clase)</li>
-          </ul>
-        </div>
         {/* Temas Clase 16 */}
         <div>
-          <h2 className='title-h2'>Temas a ver en Clase 16 - lunes 14 de julio</h2>
+          <h2 className='title-h2'>Temas a ver en Clase 17 - Lunes 15 de diciembre</h2>
           <ul className='list-disc list-inside'>
-            <li>Vamos a preparar un taller de Git-Github para versionado del código y trabajo colaborativo - 1 hora</li>
-            <li>Espacio para dudas sobre la Entrega final</li>
-          </ul>
-        </div>
-        {/* Taller Miercoles 16 */}
-        <div>
-          <h2 className='title-h2'>Taller PFI - Miércoles 16 de julio</h2>
-          <ul className='list-disc list-inside'>
-            <li>Recuerden que tendremos un After Class Especial, en el que haremos un modelo de entrega del PFI completo en un Taller de 2 horas!</li>
+            <li>Clase de cierre, compartir experiencias, opiniones, sugerencias y analizar cómo continuar el recorrido en el mundo de los datos!</li>
           </ul>
         </div>
 
